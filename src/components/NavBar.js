3da3a9ffed734b4d89ac7d600e3be6aa5de414ca
IngNex <ingnexmaicol14@gmail.com>
2023-08-16 12:49:04 -0500

    add logo and update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,44 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import { useRouter } from 'next/router';
+
+const CustomLink = ({href, title, className=""})=>{
+    const router = useRouter();
+    return(
+        <Link href={href} className={`${className} relative group`} >
+            {title}
+            <span className={`h-[1px] inline-block bg-dark 
+            absolute left-0 -bottom-0.5 group-hover:w-full 
+            transition-[width] ease duration-300 
+            ${router.asPath === href ? 'w-full':'w-0'}`}>&nbsp;</span>
+        </Link>
+    )
+}
+
 
 const NavBar = () => {
 return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/articles">Articles</Link>
+            <CustomLink href="/" title="Home" className='mr-4'/>
+            <CustomLink href="/about" title="About" className='mx-4'/>
+            <CustomLink href="/projects" title="Projects" className='mx-4'/>
+            <CustomLink href="/articles" title="Articles" className='ml-4'/>
         </nav>
-        <Logo/>
+        
         <nav>
-            <Link href="/" target={"_black"}>T</Link>
-            <Link href="/" target={"_black"}>T</Link>
-            <Link href="/" target={"_black"}>T</Link>
-            <Link href="/" target={"_black"}>T</Link>
-            <Link href="/" target={"_black"}>T</Link>
-            <Link href="/" target={"_black"}>T</Link>
-            <Link href="/" target={"_black"}>T</Link>
+            <Link href="" target={"_black"}>T</Link>
+            <Link href="" target={"_black"}>T</Link>
+            <Link href="" target={"_black"}>T</Link>
+            <Link href="" target={"_black"}>T</Link>
+            <Link href="" target={"_black"}>T</Link>
+            <Link href="" target={"_black"}>T</Link>
+            <Link href="" target={"_black"}>T</Link>
         </nav>
+        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo/>
+        </div>
     </header>
 )
 }
