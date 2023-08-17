d897214740eb57335d8a3ab5ed21f8bfa03dc5d7
IngNex <ingnexmaicol14@gmail.com>
2023-08-16 20:36:44 -0500

    add Icons in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router';
+import {motion} from "framer-motion"
+import  { TwitterIcon, GithubIcon, LinkedInIcon, PinterestIcon, DribbbleIcon } from './Icons';
 
 const CustomLink = ({href, title, className=""})=>{
     const router = useRouter();
@@ -27,14 +29,17 @@ return (
             <CustomLink href="/articles" title="Articles" className='ml-4'/>
         </nav>
         
-        <nav>
-            <Link href="" target={"_black"}>T</Link>
-            <Link href="" target={"_black"}>T</Link>
-            <Link href="" target={"_black"}>T</Link>
-            <Link href="" target={"_black"}>T</Link>
-            <Link href="" target={"_black"}>T</Link>
-            <Link href="" target={"_black"}>T</Link>
-            <Link href="" target={"_black"}>T</Link>
+        <nav className='flex items-center justify-center flex-wrap'>
+            <motion.a href="" target={"_black"} whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 mr-3">
+                <TwitterIcon/></motion.a>
+            <motion.a href="" target={"_black"} whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 mx-3">
+                <GithubIcon/></motion.a>
+            <motion.a href="" target={"_black"} whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 mx-3">
+                <LinkedInIcon/></motion.a>
+            <motion.a href="" target={"_black"} whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 mx-3">
+                <PinterestIcon/></motion.a>
+            <motion.a href="" target={"_black"} whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 ml-3">
+                <DribbbleIcon/></motion.a>
         </nav>
         <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo/>
