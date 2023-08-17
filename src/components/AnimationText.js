f29ddcc13dd5f48ee70bfa6d105ff29978cabff0
IngNex <ingnexmaicol14@gmail.com>
2023-08-16 23:09:21 -0500

    add Layout and image, info and bottom

---
+++ src/components/AnimationText.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import {motion} from 'framer-motion'
+
+const quote = {
+  initial:{
+    opacity:1,
+  },
+  animate:{
+    opacity:1,
+    transition:{
+      delay:0.5,
+      staggerChildren: 0.08,
+    }
+  }
+}
+
+const singleWord = {
+  initial:{
+    opacity:0, 
+    y:50,
+  },
+  animate:{
+    opacity:1,
+    y:0,
+    transition:{
+      duration:1,
+    }
+  }
+}
+
+const AnimationText = ({text, className=""}) => {
+  return (
+    <div className='w-full mx-auto py-2 flex item-center
+    justify-center text-center overflow-hidden'>
+      <motion.h1 
+      className={`inline-block w-full text-dark font-bold 
+      capitalize text-8xl ${className}`}
+      variants={quote}
+      initial="initial"
+      animate="animate"
+      >
+        {text.split(' ').map(
+          (word,index)=>
+        <motion.span key={word+'-'+index} className='inline-block'
+          variants={singleWord}
+          //initial="initial"
+          //animate="animate"
+        >
+          {word}&nbsp;
+        </motion.span>
+        )
+        }
+        </motion.h1>
+    </div>
+  )
+}
+
+export default AnimationText
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,6 +22,9 @@ const CustomLink = ({href, title, className=""})=>{
 const NavBar = () => {
 return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+        <Logo/>
+        </div>
         <nav>
             <CustomLink href="/" title="Home" className='mr-4'/>
             <CustomLink href="/about" title="About" className='mx-4'/>
@@ -41,9 +44,7 @@ return (
             <motion.a href="" target={"_black"} whileHover={{y:-2}} whileTap={{scale:0.9}} className="w-6 ml-3">
                 <DribbbleIcon/></motion.a>
         </nav>
-        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
-        <Logo/>
-        </div>
+        
     </header>
 )
 }
