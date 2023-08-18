38d7c41e12189fa3871bd047cbc1305be43f29a9
IngNex <ingnexmaicol14@gmail.com>
2023-08-18 17:41:17 -0500

    add about page biography

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,38 @@
 import AnimationText from '@/components/AnimationText'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
-import React from 'react'
+import Image from 'next/image'
+import React, { useEffect, useRef } from 'react'
+import profile from '../../public/images/profile/profile.jpg'
+import { useSpring, useInView, useMotionValue } from 'framer-motion'
+
+
+const AnimationNumbers =  ({value})=>{
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, {duration: 3000});
+  const isInView = useInView(ref, {once:true});
+
+  useEffect(()=>{
+    if (isInView) {
+      motionValue.set(value);
+
+    }
+  }, [isInView, value, motionValue])
+
+  useEffect(() => {
+    springValue.on("change", (latest)=>{
+      //console.log(latest)
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    })
+  }, [springValue, value])
+  
+
+  return <span ref={ref}></span>
+}
 
 const about = () => {
   return (
@@ -10,27 +41,53 @@ const about = () => {
         <title>MaicolDev | About Page</title>
         <meta name='description' content='any description'/>
       </Head>
-      <main className='flex w-full flex-col items-center justify-center '>
+      <main className='flex w-full flex-col items-center justify-center'>
         <Layout className='pt-16'>
             <AnimationText className='mb-16' text="Passion Fuels Purpose!"/>
             <div className='grid w-full grid-cols-8 gap-16'>
-                <div className='col-span-3 flex flex-col items-center justify-start'>
+                <div className='col-span-3 flex flex-col items-start justify-start'>
                     <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
                     <p className='font-medium'>
-                    Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                    Hi, I&rsquo;m CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
                     and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                    new and innovative ways to bring my clients visions to life.
+                    new and innovative ways to bring my client&rsquo;s visions to life.
                     </p>
                     <p className='my-4 font-medium'>
-                        I believe that design is about more than just making things look pretty. it's about solving problems and 
+                        I believe that design is about more than just making things look pretty. it&rsquo;s about solving problems and 
                         creating intuitive, enjoyable experiences for users. 
                     </p>
                     <p className='font-medium'>
-                        Whether I'm working on a website, mobile app, or 
+                        Whether I&rsquo;m working on a website, mobile app, or 
                         other digital product, I bring my commitment to design excellence and user-centered thinking to 
                         every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
                     </p>
                 </div>
+                <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
+                bg-light p-8'>
+                  <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'/>
+                  <Image src={profile} alt="Michael Rodriguez" className='w-full h-auto rounded-2xl'/>
+                </div>
+                <div className='col-span-2 flex flex-col items-end justify-between'>
+                  <div className='flex flex-col items-end justify-center'>
+                    <span className='inline-block text-7xl font-bold'>
+                      <AnimationNumbers value={50}/>+
+                    </span>
+                    <h2 className='text-xl font-medium capitalize text-dark/75'>satisfied clients</h2>
+                  </div>
+                  <div className='flex flex-col items-end justify-center'>
+                    <span className='inline-block text-7xl font-bold'>
+                    <AnimationNumbers value={40}/>+
+                    </span>
+                    <h2 className='text-xl font-medium capitalize text-dark/75'>projects completed</h2>
+                  </div>
+                  <div className='flex flex-col items-end justify-center'>
+                    <span className='inline-block text-7xl font-bold'>
+                    <AnimationNumbers value={2}/>+
+                    </span>
+                    <h2 className='text-xl font-medium capitalize text-dark/75'>years of experience</h2>
+                  </div>
+                </div>
+                
             </div>
         </Layout>
       </main>
