fbc777208359b3c8fa993761680521d062d7768e
IngNex <ingnexmaicol14@gmail.com>
2023-08-23 17:50:10 -0500

    add responsive page projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,6 +10,7 @@ import { motion } from 'framer-motion'
 
 const FramerImage = motion(Image);
 
+/* =============== Components FeaturedProject ================= */
 const FeaturedProject = ({type, title, summary, img, link,  github})=>{
     return (
         <motion.div
@@ -18,12 +19,14 @@ const FeaturedProject = ({type, title, summary, img, link,  github})=>{
         viewport={{once:true}}
         >
             <article className='w-full flex items-center justify-between relative rounded-br-2xl
-        rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light'
+            rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light
+            lg:flex-col lg:p-8 xs:rounded-xl xs:rounded-br-3xl xs:p-4'
         >
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] 
-            rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light'/>
+            rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light
+            xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]'/>
             <Link href={link} target="_blank"
-            className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
+            className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'
             >
                 <FramerImage src={img} alt={title} className='w-full h-auto'
                 whileHover={{scale:1.05}}
@@ -32,20 +35,21 @@ const FeaturedProject = ({type, title, summary, img, link,  github})=>{
                 sizes='(max-width: 768px) 100vw,(max-width: 1200px) 50vw 33vw'
                 />
             </Link>
-            <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-                <span className='text-primary font-medium text-xl dark:text-primaryDark'>
+            <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
+                <span className='text-primary font-medium text-xl dark:text-primaryDark xs:text-base'>
                     {type}
                 </span>
-                <Link href={link} target="_blank" className='hover:underline underline-offset-2 dark:'>
-                    <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
+                <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
+                    <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-base'>{title}</h2>
                 </Link>
-                <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
+                <p className='my-2 font-medium text-dark dark:text-light sm:text-sm'>{summary}</p>
                 <div className='mt-2 flex items-center'>
                     <Link href={github} target='_blank' className='w-10'>
                         <GithubIcon/>
                     </Link>
                     <Link href={link} target='_blank'
-                    className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-semibold dark:bg-light dark:text-dark'
+                    className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-semibold dark:bg-light dark:text-dark
+                    sm:px-4 sm:text-base'
                     >
                         Visit Project
                     </Link>
@@ -58,6 +62,7 @@ const FeaturedProject = ({type, title, summary, img, link,  github})=>{
     )
 }
 
+/* =============== Components Project ================= */
 const Project = ({title, type, img, link, github})=>{
     return(
         <motion.div
@@ -66,9 +71,11 @@ const Project = ({title, type, img, link, github})=>{
         viewport={{once:true}}
         >
 <article className='w-full flex flex-col items-center justify-center rounded-2xl
-        border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light'>
+        border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light
+        xs:p-4'>
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] 
-            rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light'/>
+            rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light
+            md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]'/>
             <Link href={link} target="_blank"
             className='w-full cursor-pointer overflow-hidden rounded-lg'
             >
@@ -80,19 +87,21 @@ const Project = ({title, type, img, link, github})=>{
                 />
             </Link>
             <div className='w-full flex flex-col items-start justify-between mt-4'>
-                <span className='text-primary font-medium text-xl dark:text-primaryDark'>
+                <span className='text-primary font-medium text-xl dark:text-primaryDark
+                lg:text-lg md:text-base '>
                     {type}
                 </span>
                 <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-                    <h2 className='my-2 w-full text-left text-3xl font-bold'>{title}</h2>
+                    <h2 className='my-2 w-full text-left text-3xl font-bold
+                    lg:text-2xl'>{title}</h2>
                 </Link>
                 <div className='w-full mt-2 flex items-center justify-between'>
                     <Link href={link} target='_blank'
-                    className='text-lg text-semibold underline'
+                    className='text-lg text-semibold underline md:text-base'
                     >
                         Visit
                     </Link>
-                    <Link href={github} target='_blank' className='w-8'>
+                    <Link href={github} target='_blank' className='w-8 md:w-6'>
                         <GithubIcon/>
                     </Link>
                     
@@ -104,6 +113,8 @@ const Project = ({title, type, img, link, github})=>{
     )
 }
 
+
+/* ================ Page Projects ==================== */
 const projects = () => {
   return (
     <>
@@ -114,8 +125,8 @@ const projects = () => {
       <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
             <AnimationText text="Imagination Trumps Knowledge!"
-            className='mb-16'/>
-            <div className='grid grid-cols-12 gap-24 gap-y-32'>
+            className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'/>
+            <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
                 <div className='col-span-12'>
                     <FeaturedProject
                     title="Crypto Screener Application"
@@ -128,7 +139,7 @@ const projects = () => {
                     github="/"
                     />
                 </div>
-                <div className='col-span-6'>
+                <div className='col-span-6 sm:col-span-12'>
                     
                     <Project
                     title="Crypto Screener Application"
@@ -138,7 +149,7 @@ const projects = () => {
                     github="/"
                     />
                 </div>
-                <div className='col-span-6'>
+                <div className='col-span-6 sm:col-span-12'>
                 <Project
                     title="Crypto Screener Application"
                     img={project1}
@@ -159,7 +170,7 @@ const projects = () => {
                     github="/"
                     />
                 </div>
-                <div className='col-span-6'>
+                <div className='col-span-6 sm:col-span-12'>
                 <Project
                     title="Crypto Screener Application"
                     img={project1}
@@ -168,7 +179,7 @@ const projects = () => {
                     github="/"
                     />
                 </div>
-                <div className='col-span-6'>
+                <div className='col-span-6 sm:col-span-12'>
                 <Project
                     title="Crypto Screener Application"
                     img={project1}
